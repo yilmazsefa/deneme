c4dc8fb0f506038228c59dcb5183d0490360b26d
Sefa Emirhan Yılmaz <96588662+yilmazsefa@users.noreply.github.com>
2022-01-13 20:17:06 +0300

    Update randomsecuritypassword.py

--- randomsecuritypassword.py
+++ randomsecuritypassword.py
@@ -5,9 +5,5 @@ from random import choice
 
 characters = string.ascii_letters + string.punctuation + string.digits
 
-a=1
-
-while a>0:
-    password = "".join(choice(characters) for x in range(0,8))
-    print(password)
-    a-=1
+password = "".join(choice(characters) for x in range(0,8))
+print(password)
